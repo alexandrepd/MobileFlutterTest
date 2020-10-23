2b68c9f53343fe349a3d8e299ab7113e9e1f86e7
Alexandre Oliveira <alexandre.oliveira@Admins-MacBook-Pro.local>
2020-10-23 00:49:59 -0300

    Acessando API

---
+++ lib/modules/base/baseProvider.dart
@@ -0,0 +1,39 @@
+import 'package:MobileFlutterTest/modules/base/defaultModel.dart';
+import 'package:graphql/client.dart';
+
+class BaseProvider {
+  final String _token = "fliperdevtest2020";
+  final String _endpoint =
+      "https://harura-fliper-test.herokuapp.com/v1/graphql";
+
+  Future<DefaultModel<T>> readGraphQl<T>(
+      String query, T Function(dynamic readGraph) callback) async {
+    DefaultModel<T> resultObject = DefaultModel<T>();
+
+    resultObject.isSuccess = false;
+
+    final HttpLink _httpLink =
+        HttpLink(uri: _endpoint, headers: {'x-hasura-access-key': _token});
+
+    final GraphQLClient _client = GraphQLClient(
+      cache: InMemoryCache(),
+      link: _httpLink,
+    );
+
+    final QueryOptions options = QueryOptions(documentNode: gql(query));
+
+    final QueryResult result = await _client.query(options);
+
+    if (result.hasException) {
+      resultObject.isSuccess = false;
+      resultObject.error = result.exception;
+    }
+
+    if (result.data != null) {
+      resultObject.isSuccess = true;
+      resultObject.data = callback(result.data);
+    }
+
+    return resultObject;
+  }
+}
